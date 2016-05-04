a0c618d4db7e49451534ea6f6aaa4b95cfcbaa64
Alan19 <alanxiao44@gmail.com>
2016-05-04 14:42:27 -0400

    Basic Forms and Blank Template

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<!-- Zhen Xiao -->
+<html>
+	<head>
+		<title></title>
+		<style></style>
+		<script></script>
+	</head>
+	<body>
+		<?php
+			echo "<h1>Hello World</h1>";
+		?>
+		<ol>
+			<li><a href = "Formtest.php">Form Test</a></li>
+	</body>
+</html>
