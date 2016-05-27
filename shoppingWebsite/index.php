8fa0e9995882abd9eabc129688e9ab7e9aba0fae
Alan19 <alanxiao44@gmail.com>
2016-05-27 14:44:23 -0400

    Started Work on Shopping Website and Receipt Generator

---
+++ shoppingWebsite/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<!-- Zhen Xiao -->
+<html>
+	<head>
+		<title>Using JSON to enchance PHP</title>
+		<style>
+			#mydata{
+				font:16pt times new roman;
+				vertical-align:middle;
+				margin:auto;
+			}
+			table,td{
+				border-collapse:collapse;
+				border-style:solid;
+				vertical-align:middle;
+				text-align: center;
+				padding:3px;
+			}
+			html{
+				background-size:cover;
+				background-repeat: no-repeat;
+
+			}
+		</style>
+		<script>
+			<?php
+				$conn = new PDO("mysql:host=localhost;dbname=shoppinglist", "root", "");
+				$cmd = "SELECT * from tuesdayslist";
+				$result = $conn->prepare($cmd);
+				$result->execute();
+
+				$data = $result->fetchAll(PDO::FETCH_NUM);
+				echo "mydata = " . json_encode($data) . ";";
+			 ?>
+			 function initialize(){
+				 outputBox = document.getElementById("mydata");
+				 theOutput = "";
+				 for(var r = 0; r < mydata.length; r++){
+					 var row1 = outputBox.insertRow(r);
+					 for(var c = 0; c < mydata[0].length; c++){
+						 var cell1 = row1.insertCell(c);
+						 cell1.innerHTML = mydata[r][c];
+					 }
+					 var quantityCell = row1.insertCell(row1.length);
+					 quantityCell.innerHTML = "<form action='receipt.php' method='post'><input type = 'number' placeholder = 'Number of Items' name = '" + r + "' min = '0' step = '1'>";
+				 }
+			 }
+		</script>
+	</head>
+	<body onload = "initialize()">
+		<table id = "mydata"></table>
+		<input type = "submit">
+	</body>
+</html>
---
+++ shoppingWebsite/receipt.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<!-- Zhen Xiao -->
+<html>
+	<head>
+		<title></title>
+		<style></style>
+		<script></script>
+	</head>
+	<body></body>
+</html>
