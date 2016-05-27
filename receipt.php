da6a213811b597aed7431f7c7bfde228186b769a
Alan19 <alanxiao44@gmail.com>
2016-05-27 14:50:11 -0400

    Added Quantity Array

---
+++ receipt.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<!-- Zhen Xiao -->
+<html>
+	<head>
+		<title>Receipt</title>
+		<style></style>
+		<script></script>
+	</head>
+	<body>
+		<?php
+			print_r($_POST);
+		?>
+	</body>
+</html>
